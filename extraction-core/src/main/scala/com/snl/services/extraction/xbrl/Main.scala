3b47608d120d4005cc4951aaa276ea853d619a01
Galen Warren <galen@cvillewarrens.com>
2015-01-21 21:07:29 -0500

    Initial setup

---
+++ extraction-core/src/main/scala/com/snl/services/extraction/xbrl/Main.scala
@@ -0,0 +1,14 @@
+package com.snl.services.extraction.xbrl
+
+import akka.actor._
+
+/**
+ * The main actor for the extraction 
+ */
+class Main extends Actor {
+
+  def receive = {
+    case _ => {}
+  }
+  
+}
